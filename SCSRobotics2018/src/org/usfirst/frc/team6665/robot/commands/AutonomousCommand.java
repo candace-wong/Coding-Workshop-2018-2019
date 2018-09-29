ae72bc0b616ecdebd1f0adeed01bef068129f1bb
candace-wong <43707010+candace-wong@users.noreply.github.com>
2018-09-29 11:49:24 -0700

    Update AutonomousCommand.java

--- SCSRobotics2018/src/org/usfirst/frc/team6665/robot/commands/AutonomousCommand.java
+++ SCSRobotics2018/src/org/usfirst/frc/team6665/robot/commands/AutonomousCommand.java
@@ -55,30 +55,19 @@ public class AutonomousCommand extends Command {
     @Override
     protected void execute() {
     	
-    	if(timer.get()<=4) {
+    	if(timer.get()<= 4) {
     	   System.err.println("autoEx ran");
-        //while(timer.get() <= 3) {
-        	Robot.driveTrain.differentialDrive1.arcadeDrive(-0.7,0);//drive "forward"
-        	// (speed/output from motor,rotation angle)
-        	}
-    	
-    ;
-		//if(timer.get()==6&&gameData.charAt(0)=='L'&&side==1){
-    		//Robot.claw.clawOpen();
-    	//}
-		//else if(timer.get()==6&&gameData.charAt(0)=='R'&&side==2) {
-			//Robot.claw.clawOpen();
-		//}
-        }
-       // while(timer.get() <= (time + 2)) {
-        	//Robot.driveTrain.differentialDrive1.arcadeDrive(0, 0.5); //rotate
-        	//time = timer.get();
-        //}
-       // while(timer.get() <= (time + 10)) {
-        	//Robot.driveTrain.differentialDrive1.arcadeDrive(0, 0); //don't move
-       // }
-    //}
-
+		
+           Robot.driveTrain.differentialDrive1.arcadeDrive(-0.6,0);//drive "forward"
+        			// (speed/output from motor,rotation angle)
+        	
+         } else {
+		Robot.driveTrain.differentialDrive1.arcadeDrive(0,0); //stop movement
+	}
+    
+      } //end v. execute
+       
+	
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
